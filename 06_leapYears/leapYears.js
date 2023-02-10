b6a88e15cbfcaf3a7ced64fe52e265251e86c35a
Buh90 <83811198+Buh90@users.noreply.github.com>
2023-02-10 13:00:41 +0100

    Do exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  switch (true) {
+    case year % 4 !== 0:
+    case year % 100 == 0 && year % 100 == 0 && year % 400 != 0:
+      return false;
+      break;
+    default:
+      return true;
+  }
 };
 
 // Do not edit below this line
